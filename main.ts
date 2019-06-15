8757260ad7daaa610552d667213bee6fea43a133
vahid <v.heidary13@gmail.com>
2019-06-15 15:08:11 +0430

    meteo authenticate

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 export class main {
     public static appid:string = '3d4f37e5d802e3882a03c80fd423f59b';
     public static Authorization:string = 'Basic c2FtYWNvbnRyb2xfZWJhZGk6MkwxUnlQUzhqbEt3cg==';
-    public static meteoAuthorization:string = 'Basic cmF5YW5faGVpZGFyeTpYNXhtNm5BcloyU3BP';
+    public static meteoAuthorization:string = 'Basic c2FtYWNvbnRyb2xfZWJhZGk6MkwxUnlQUzhqbEt3cg==';
     public static agro_url:string = 'https://api.agromonitoring.com/agro/1.0/';
     public static meteo_url: string = 'https://api.meteomatics.com/';
 }
